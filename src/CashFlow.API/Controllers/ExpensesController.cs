b9aaf9447c857587037d0ba47b3b08016899c618
Giovani <giovani98@proton.me>
2024-06-16 10:11:56 -0300

    feat: Create business class for expense registration

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,3 +1,4 @@
+using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,9 +8,13 @@ namespace CashFlow.API.Controllers;
 [ApiController]
 public class ExpensesController : ControllerBase
 {
-  [HttpPost]
-  public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
-  {
-    return Created();
-  }
+	[HttpPost]
+	public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+	{
+		var useCase = new RegisterExpenseUseCase();
+
+		var response = useCase.Execute(request);
+
+		return Created(string.Empty, response);
+	}
 }
