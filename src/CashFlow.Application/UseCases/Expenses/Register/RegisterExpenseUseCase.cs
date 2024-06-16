1497094103cb5fcdd73d5403a8d09488c2e0e011
Giovani <giovani98@proton.me>
2024-06-16 13:28:45 -0300

    refact: Replace validations with Fluent Validation

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,31 +1,31 @@
 using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using FluentValidation;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 
 public class RegisterExpenseUseCase
 {
-    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request) {
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
         Validate(request);
-        
+
         return new ResponseRegisteredExpenseJson();
     }
 
-    private void Validate(RequestRegisterExpenseJson request) {
-        var isTitleEmpty = string.IsNullOrWhiteSpace(request.Title);
-        if (isTitleEmpty)
-            throw new ArgumentException("The title is required,");
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var validator = new RegisterExpenseValidator();
+        var result = validator.Validate(request);
 
-        if (request.Amount <= 0)
-            throw new ArgumentException("The amount must be greater than zero");
-        
-        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-        if (result > 0)
-            throw new ArgumentException("Expenses cannot be in the future.");
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
 
-        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymenType);
-        if (!paymentTypeIsValid)
-            throw new ArgumentException("The informed payment type is invalid.");
+            // Criar exception customizada para receber errorMessages
+            throw new ArgumentException(errorMessages);
+        }
+
     }
 }
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application;
+
+public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
+{
+    public RegisterExpenseValidator()
+    {
+        RuleFor(expense => expense.Title).NotEmpty().WithMessage("The title is required.");
+        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("The amount must be greater than zero");
+        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Expenses cannot be in the future.");
+        RuleFor(expense => expense.PaymenType).IsInEnum().WithMessage("The informed payment type is invalid.");
+    }
+}
