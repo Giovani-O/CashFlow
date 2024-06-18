91b7e18b3cc50ac781b950e69b8ff3f5583f670d
Giovani <giovani98@proton.me>
2024-06-18 14:37:04 -0300

    feat: Create xUnit tests project for validator

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -11,6 +11,6 @@ public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpense
         RuleFor(expense => expense.Title).NotEmpty().WithMessage(ResourceErrorMessages.TITLE_IS_REQUIRED);
         RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
         RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.EXPENSES_CANNOT_BE_IN_THE_FUTURE);
-        RuleFor(expense => expense.PaymenType).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_IS_INVALID);
+        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_IS_INVALID);
     }
 }
---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using CashFlow.Application;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "Description",
+            Title = "Apple",
+            PaymentType = PaymentType.CreditCard
+        };
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+}
