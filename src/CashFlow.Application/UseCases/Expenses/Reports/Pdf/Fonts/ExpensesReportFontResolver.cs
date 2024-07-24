37c7f9e7eb1144a834043e2dddd77d761a9f4806
Giovani <giovani98@proton.me>
2024-07-24 17:20:54 -0300

    feat: WiP - PDF generation

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -1,15 +1,29 @@
-﻿using PdfSharp.Fonts;
+﻿using System.Reflection;
+using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 public class ExpensesReportFontResolver : IFontResolver
 {
     public byte[]? GetFont(string faceName)
     {
-        throw new NotImplementedException();
+        var stream = ReadFontFile(faceName) ?? ReadFontFile(FontHelper.DEFAULT_FONT);
+
+        var length = (int)stream!.Length;
+        var data = new byte[length];
+        stream.Read(buffer: data, offset: 0, count: length);
+
+        return data;
     }
 
     public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
     {
         return new FontResolverInfo(familyName);
+    }
+
+    private Stream? ReadFontFile(string faceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        return assembly.GetManifestResourceStream($"CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts.{faceName}.ttf");
     }
 }
--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,6 +1,12 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
+using DocumentFormat.OpenXml.Wordprocessing;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
 using PdfSharp.Fonts;
+using Document = MigraDoc.DocumentObjectModel.Document;
+using Font = MigraDoc.DocumentObjectModel.Font;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
@@ -22,9 +28,72 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
         {
             return [];
         }
+        
+        var document = CreateDocument(month);
+        var page = CreatePage(document);
 
+        var table = page.AddTable();
+        table.AddColumn();
+        table.AddColumn();
+        
+        var row = table.AddRow();
+        row.Cells[0].AddImage("/home/murasaki/github/CashFlow/src/CashFlow.Domain/Assets/user.png");
+
+        row.Cells[1].AddParagraph("Hey, Giovani de Oliveira");
+        row.Cells[1].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 16 };
         
 
-        return [];
+        var paragraph = page.AddParagraph();
+        var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
+        paragraph.AddFormattedText(title, new Font{ Name = FontHelper.RALEWAY_REGULAR, Size = 15});
+        
+        paragraph.AddLineBreak();
+
+        var totalExpenses = expenses.Sum(expense => expense.Amount);
+        paragraph.AddFormattedText(
+            $"{totalExpenses} {CURRENCY_SYMBOL}", new Font{ Name = FontHelper.WORKSANS_BLACK, Size = 50});
+        
+        return RenderDocument(document);
+    }
+
+    private Document CreateDocument(DateOnly month)
+    {
+        var document = new Document();
+        document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSES_FOR} {month:Y}";
+        document.Info.Author = "Giovani";
+
+        var style = document.Styles["Normal"];
+        style!.Font.Name = FontHelper.RALEWAY_REGULAR;
+        
+        return document;
+    }
+
+    private Section CreatePage(Document document)
+    {
+        var section = document.AddSection();
+        section.PageSetup = document.DefaultPageSetup.Clone();
+        
+        section.PageSetup.PageFormat = PageFormat.A4;
+        section.PageSetup.LeftMargin = 40;
+        section.PageSetup.RightMargin = 40;
+        section.PageSetup.TopMargin = 80;
+        section.PageSetup.BottomMargin = 80;
+        
+        return section;
+    }
+
+    private byte[] RenderDocument(Document document)
+    {
+        var renderer = new PdfDocumentRenderer
+        {
+            Document = document
+        };
+        
+        renderer.RenderDocument();
+        
+        using var file = new MemoryStream();
+        renderer.PdfDocument.Save(file);
+        
+        return file.ToArray();
     }
 }
