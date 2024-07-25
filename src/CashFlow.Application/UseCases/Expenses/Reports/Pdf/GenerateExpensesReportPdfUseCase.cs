e60d492244dabfced6ab233385de9b4f9d90d21e
Giovani <giovani98@proton.me>
2024-07-25 15:47:06 -0300

    feat: WiP - PDF Generation

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,12 +1,20 @@
-﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+﻿using System.Reflection;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Colors;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
+using DocumentFormat.OpenXml.Drawing.Charts;
 using DocumentFormat.OpenXml.Wordprocessing;
 using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Shapes;
+using MigraDoc.DocumentObjectModel.Tables;
 using MigraDoc.Rendering;
 using PdfSharp.Fonts;
+using Columns = DocumentFormat.OpenXml.Wordprocessing.Columns;
 using Document = MigraDoc.DocumentObjectModel.Document;
 using Font = MigraDoc.DocumentObjectModel.Font;
+using Paragraph = DocumentFormat.OpenXml.Wordprocessing.Paragraph;
+using Table = MigraDoc.DocumentObjectModel.Tables.Table;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
@@ -32,26 +40,18 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
         var document = CreateDocument(month);
         var page = CreatePage(document);
 
-        var table = page.AddTable();
-        table.AddColumn();
-        table.AddColumn();
-        
-        var row = table.AddRow();
-        row.Cells[0].AddImage("/home/murasaki/github/CashFlow/src/CashFlow.Domain/Assets/user.png");
-
-        row.Cells[1].AddParagraph("Hey, Giovani de Oliveira");
-        row.Cells[1].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 16 };
-        
-
-        var paragraph = page.AddParagraph();
-        var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
-        paragraph.AddFormattedText(title, new Font{ Name = FontHelper.RALEWAY_REGULAR, Size = 15});
-        
-        paragraph.AddLineBreak();
+        CreateHeaderWithProfilePictureAndName(page);
 
         var totalExpenses = expenses.Sum(expense => expense.Amount);
-        paragraph.AddFormattedText(
-            $"{totalExpenses} {CURRENCY_SYMBOL}", new Font{ Name = FontHelper.WORKSANS_BLACK, Size = 50});
+        
+        CreateTotalSpentSection(page, month, totalExpenses);
+
+        foreach (var expense in expenses)
+        {
+            var table = CreateExpensesTable(page);
+
+            new Font { Color = ColorsHelper.RED_LIGHT };
+        }
         
         return RenderDocument(document);
     }
@@ -80,6 +80,54 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
         section.PageSetup.BottomMargin = 80;
         
         return section;
+    }
+
+    private void CreateHeaderWithProfilePictureAndName(Section page)
+    {
+        var table = page.AddTable();
+        table.AddColumn();
+        table.AddColumn("300");
+        
+        var row = table.AddRow();
+        
+        var assembly = Assembly.GetExecutingAssembly();
+        var directoryName = Path.GetDirectoryName(assembly.Location);
+        var filePath = Path.Combine(directoryName!, "Logo", "user.png");
+        
+        row.Cells[0].AddImage(filePath);
+
+        row.Cells[1].AddParagraph("Hey, Giovani de Oliveira");
+        row.Cells[1].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 16 };
+        row.Cells[1].VerticalAlignment = VerticalAlignment.Center;
+    }
+
+    private void CreateTotalSpentSection(Section page, DateOnly month, decimal totalExpenses)
+    {
+        var paragraph = page.AddParagraph();
+
+        paragraph.Format.SpaceBefore = "40";
+        paragraph.Format.SpaceAfter = "40";
+        
+        var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
+        paragraph.AddFormattedText(title, new Font{ Name = FontHelper.RALEWAY_REGULAR, Size = 15});
+        
+        paragraph.AddLineBreak();
+
+        
+        paragraph.AddFormattedText(
+            $"{totalExpenses} {CURRENCY_SYMBOL}", new Font{ Name = FontHelper.WORKSANS_BLACK, Size = 50});
+    }
+
+    private Table CreateExpensesTable(Section page)
+    {
+        var table = page.AddTable();
+
+        table.AddColumn("195").Format.Alignment = ParagraphAlignment.Left;
+        table.AddColumn("80").Format.Alignment = ParagraphAlignment.Center;
+        table.AddColumn("120").Format.Alignment = ParagraphAlignment.Center;
+        table.AddColumn("120").Format.Alignment = ParagraphAlignment.Right;
+        
+        return table;
     }
 
     private byte[] RenderDocument(Document document)
