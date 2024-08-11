061e46a6229c0cb5112001a5c13e26c028931591
Giovani <giovani98@proton.me>
2024-08-10 21:09:32 -0300

    feat: feat: Starts user controller and business logic

---
+++ src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
@@ -0,0 +1,15 @@
+using FluentValidation;
+using FluentValidation.Validators;
+
+namespace CashFlow.Application.UseCases.Users;
+
+public class PasswordValidator<T> : PropertyValidator<T, string>
+{
+    public override string Name => "PasswordValidator";
+    
+    public override bool IsValid(ValidationContext<T> context, string password)
+    {
+        // TO DO
+        throw new NotImplementedException();
+    }
+}
